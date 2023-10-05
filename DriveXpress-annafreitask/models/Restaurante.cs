64c2be30dd64c77c2ffa1b877e9680dfc58add4f
Anna Freitas <102244252+AnnaFreitask@users.noreply.github.com>
2023-10-04 23:31:47 -0300

    Criação e configuração dos modelos de DriverThru.

---
+++ DriveXpress-annafreitask/models/Restaurante.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DriveXpress_annafreitask.modells
+{
+    [Table("Restaurant")]
+    public class Restaurante
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public String phone { get; set; }
+        [Required]
+        public string address { get; set; }
+        [Required]
+        public Categoria categoria { get; set; }
+
+        public ICollection<cardapio> Cardapio { get; set; }
+    }
+
+
+
+}
+
---
+++ DriveXpress-annafreitask/models/cardapio.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DriveXpress_annafreitask.modells
+{
+    [Table("Cardapio")]
+    public class cardapio
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string nome { get; set; }
+        [Required]
+        public int Quantidad { get; set; }
+        [Required]
+        public string description { get; set; }
+        [Required]
+        public double value { get; set; }
+        [Required]
+        public Cardapio food { get; set; }
+
+        [Required]
+        public int RestauranteId { get; set; } //produto pertence a um unico restaurante (produto associado a Key de restaurante)
+
+        public Restaurante Restaurante { get; set; } //retorna dados restaurante
+    }
+
+    public enum Cardapio { 
+    comida,
+    bebida
+    }
+
+
+}
